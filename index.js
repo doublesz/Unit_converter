314cb2a35afd4fff83450356b8fa8f53f83b57cb
doublesz <sz.suchodolski@gmail.com>
2023-02-20 07:13:50 +0100

    fix issue with singulara and plural forms of units

--- index.js
+++ index.js
@@ -5,9 +5,6 @@
 */
 
 // todo -> think how to convert unit names to its singular form
-// allow only number input
-// parse input value before calculation
-// remove arrows from input box
 
 const inputEl = document.querySelector('.input-el');
 const convertBtn = document.querySelector('.convert-btn');
@@ -19,24 +16,48 @@ let val = 0;
 
 const lenObj = {
     unit1: 'meters',
+    unit1s: 'meter',
     unit2: 'feet',
+    unit2s: 'foot',
     rate: 3.281
 }
 
 const volObj = {
     unit1: 'liters',
+    unit1s: 'liter',
     unit2: 'gallons',
+    unit2s: 'gallon',
     rate: 0.264
 }
 
 const massObj = {
     unit1: 'kilos',
+    unit1s: 'kilo',
     unit2: 'pounds',
+    unit2s: 'pound',
     rate: 2.204
 }
 
 const genTxt = (obj) => {
-    return `${val} ${obj.unit1} = ${(val * obj.rate).toFixed(3)} ${obj.unit2} | ${val} ${obj.unit2} = ${(val / obj.rate).toFixed(3)} ${obj.unit1}`
+    let result1 = (val * obj.rate).toFixed(3);
+    let result2 = (val / obj.rate).toFixed(3);
+    let sentence = '';
+
+    switch (1.0){
+        case val:
+            sentence = `${val} ${obj.unit1s} = ${result1} ${obj.unit2} | ${val} ${obj.unit2s} = ${result2} ${obj.unit1}`;
+            break;
+        case parseFloat(result1):
+            sentence = `${val} ${obj.unit1} = ${result1} ${obj.unit2s} | ${val} ${obj.unit2} = ${result2} ${obj.unit1}`;
+            break;
+        case parseFloat(result2):
+            sentence = `${val} ${obj.unit1} = ${result1} ${obj.unit2} | ${val} ${obj.unit2} = ${result2} ${obj.unit1s}`;
+            break;
+        default:
+            sentence = `${val} ${obj.unit1} = ${result1} ${obj.unit2} | ${val} ${obj.unit2} = ${result2} ${obj.unit1}`
+    }
+
+    return sentence;
 }
 
 
@@ -58,10 +79,10 @@ const massP = document.querySelector('.mass p');
 
 const calculateUnits = () => {
     val = parseFloat(inputEl.value);
-    
     lenP.innerText = genTxt(lenObj);
     volP.innerText = genTxt(volObj);
     massP.innerText = genTxt(massObj);
+    inputEl.value = "";
 }
 
 convertBtn.addEventListener('click', calculateUnits);
